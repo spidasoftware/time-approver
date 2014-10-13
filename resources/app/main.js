6049d7cec65ca90812ee55fd5e0cbfce7d08cd34
Timothy Overly <timothy.overly@gmail.com>
2014-10-13 14:54:29 -0400

    Try at app

---
+++ resources/app/main.js
@@ -0,0 +1,49 @@
+var app = require('app');
+var Menu = require('menu');
+var MenuItem = require('menu-item');
+var BrowserWindow = require('browser-window');
+
+var mainWindow = null;
+var menu = null;
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+  app.quit();
+});
+
+app.on('ready', function() {
+  app.commandLine.appendSwitch('js-flags', '--harmony_collections');
+
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    resizable: false,
+    'use-content-size': true,
+  });
+  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+  mainWindow.focus();
+
+     var template = [
+      {
+        label: 'File',
+        submenu: [
+          {
+            label: 'Review',
+            click: function() { mainWindow.close(); }
+          },          
+          {
+            label: 'Approve',
+            click: function() { mainWindow.close(); }
+          },
+          {
+            label: 'Close',
+            click: function() { mainWindow.close(); }
+          },
+        ]
+      }
+    ];
+
+    menu = Menu.buildFromTemplate(template);
+    mainWindow.setMenu(menu);
+  
+});
