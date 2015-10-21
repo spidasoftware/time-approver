e90219976d7e460816f8f453060e5334be4f93f7
Timothy Overly <timothy.overly@gmail.com>
2015-10-21 12:31:45 -0400

    Working archives

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,20 +33,22 @@ module.exports = function(grunt) {
       },
       'renameExe':{
         cmd: 'mv "build/win32/electron.exe" "build/win32/time-approver.exe"'
+      },
+      'zipWin' : {
+        cmd: 'cd build/win32;zip -r windows-time-approver.zip .;mkdir ../../dist;mv windows-time-approver.zip ../../dist/windows-time-approver.zip'
+      },
+      'zipOsx' : {
+        cmd: 'cd build/darwin;tar -zcvf osx-time-approver.tar.gz .;mv osx-time-approver.tar.gz ../../dist/osx-time-approver.tar.gz'
       }
     },
-    'zip' : {
-      'dist/windows-time-approver.zip': ['build/win32/**'],
-      'dist/osx-time-approver.zip': ['build/darwin/**']
-    }
   });
 
   grunt.loadNpmTasks('grunt-electron-app-builder');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-exec');
-  grunt.loadNpmTasks('grunt-zip');
 
   grunt.registerTask('rename', ['exec:copyIcon', 'exec:renameApp', 'exec:renameExe']);
+  grunt.registerTask('zip', ['exec:zipWin', 'exec:zipOsx']);
   grunt.registerTask('default', ['clean', 'copy', 'exec:install', 'build-electron-app', 'rename', 'zip']);
 };
